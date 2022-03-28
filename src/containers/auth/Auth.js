57e87183a010ce7ce86a3acffd8ef04de27653b5
Edward-Watts <edwardwadjei@gmail.com>
2022-03-28 10:21:22 +0000

    initial commit

---
+++ src/containers/auth/Auth.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const auth = (props) => {
+
+}
+
+
+export default auth;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+
+class Layout extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>Pam</h1>
+            </div>
+        );
+    }
+}
+
+export default Layout;
