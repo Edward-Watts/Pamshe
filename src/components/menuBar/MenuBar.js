8d25c0cb406094bcbaab9dd71b2aba25fe16e4f6
Edward-Watts <edwardwadjei@gmail.com>
2022-04-12 03:03:06 +0000

    feature(): implement menu bar

--- src/components/menuBar/MenuBar.js
+++ src/components/menuBar/MenuBar.js
@@ -1,29 +1,39 @@
 import React from "react";
+import { Container, Dropdown } from "react-bootstrap";
 
 import './MenuBar.css'
 
 
 const menuBar = () => {
     return (
-        <div className="menuBar d-flex align-items-center">
-            <div>
-                <h1>Pamshe</h1>
-            </div>
-            <div>
-                <h6>Category</h6>
-            </div>
-            <div>
-                <h6>Category</h6>
-            </div>
-            <div>
-                <h6>Search</h6>
-            </div>
-            <div>
-                <h6>User</h6>
-            </div>
-            <div>
-                <h6>Cart</h6>
-            </div>
+        <div className="menuBar d-flex align-items-center justify-content-center">
+            <Container className="sub d-flex align-items-center justify-content-center">
+                <div className="item mx-1">
+                    <Dropdown>
+                        <Dropdown.Toggle variant="transparent" id="dropdown-basic">
+                            Category
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div className="item mx-1">
+                    <h6>Customize</h6>
+                </div>
+                <div className="search mx-1">
+                    <h6>Insert search component</h6>
+                </div>
+                <div className="item mx-1">
+                    <h6>User</h6>
+                </div>
+                <div className="item mx-1">
+                    <h6>Cart</h6>
+                </div>
+            </Container>
         </div>
     ) 
 }
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Carousel} from 'react-bootstrap';
+import {Carousel, Container, Card, Button} from 'react-bootstrap';
 
 import './Home.css'
 import MenuBar from "../../components/menuBar/MenuBar";
@@ -55,6 +55,21 @@ class Home extends Component {
                             </Carousel.Item>
                             ))}
                         </Carousel>
+                </div>
+                <div className="cardContainer">
+                    <Container>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Container>
                 </div>
             </div>
         )
